997e0d91edc745abf33eaa41d244bafa58a1b298
littleturtleOwO <bob20050412@gmail.com>
2022-01-21 18:24:53 +0800

    2021/01/22-8

--- bot.js
+++ bot.js
@@ -26,16 +26,16 @@
             return;
         var rnd = Radom(10,1) ;
         switch(rnd){
-            case 1:msg.reply('你好') ;break ;
-            case 2:msg.reply('おはようございます') ;break ;
-            case 3:msg.reply('こんにちは') ;break ;
-            case 4:msg.reply('アンアン') ;break ;
-            case 5:msg.reply('安安') ;break ;
-            case 6:msg.reply('早安') ;break ;
-            case 7:msg.reply('午安') ;break ;
-            case 8:msg.reply('晚安') ;break ;
-            case 9:msg.reply('hi') ;break ;
-            case 10:msg.reply('hi owo') ;break ;
+            case 1:msg.channel.send('你好') ;break ;
+            case 2:msg.channel.send('おはようございます') ;break ;
+            case 3:msg.channel.send('こんにちは') ;break ;
+            case 4:msg.channel.send('アンアン') ;break ;
+            case 5:msg.channel.send('安安') ;break ;
+            case 6:msg.channel.send('早安') ;break ;
+            case 7:msg.channel.send('午安') ;break ;
+            case 8:msg.channel.send('晚安') ;break ;
+            case 9:msg.channel.send('hi') ;break ;
+            case 10:msg.channel.send('hi owo') ;break ;
             }
         }
         
@@ -55,36 +55,36 @@
            var num = Radom(30,1) ;
         switch(num){
         
-            case 1:msg.send('當然') ;break ;
-            case 2:msg.send('你不會失望的') ;break ;
-            case 3:msg.sendy('及早開始') ;break ;
-            case 4:msg.send('走一步算一步') ;break ;
-            case 5:msg.send('很可能遭遇不幸') ;break ;
-            case 6:msg.send('無論你做什麼 結果將是持久的') ;break ;
-            case 7:msg.send('那超出了你的控制範圍') ;break ;
-            case 8:msg.send('慷慨大方一點') ;break ;
-            case 9:msg.send('絕對不是') ;break ;
-            case 10:msg.send('也許') ;break ;
-            case 11:msg.send('它將會帶來好運') ;break ;
-            case 12:msg.send('你需要考慮其他方法') ;break ;
-            case 13:msg.send('這不值得爭取') ;break ;
-            case 14:msg.send('你的行為將會改善這件事') ;break ;
-            case 15:msg.send('這是毫無疑問的') ;break ;
-            case 16:msg.send('你必須現在行動') ;break ;
-            case 17:msg.send('絕不') ;break ;
-            case 18:msg.send('這不重要') ;break ;
-            case 19:msg.sendy('毫無疑問') ;break ;
-            case 20:msg.send('不') ;break ;
-            case 21:msg.send('你看得太近了') ;break ;
-            case 22:msg.send('尋找更多選擇') ;break ;
-            case 23:msg.send('你不是真心在意') ;break ;
-            case 24:msg.send('這具有重大意義') ;break ;
-            case 25:msg.send('信你的直覺') ;break ;
-            case 26:msg.send('船到橋頭自然直') ;break ;
-            case 27:msg.send('船到橋頭自然沉') ;break ;
-            case 28:msg.send('等待一個更好的時機') ;break ;
-            case 29:msg.send('等待一個更好的時機') ;break ;
-            case 30:msg.send('owo') ;break ;
+            case 1:msg.channel.send('當然') ;break ;
+            case 2:msg.channel.send('你不會失望的') ;break ;
+            case 3:msg.channel.sendy('及早開始') ;break ;
+            case 4:msg.channel.send('走一步算一步') ;break ;
+            case 5:msg.channel.send('很可能遭遇不幸') ;break ;
+            case 6:msg.channel.send('無論你做什麼 結果將是持久的') ;break ;
+            case 7:msg.channel.send('那超出了你的控制範圍') ;break ;
+            case 8:msg.channel.send('慷慨大方一點') ;break ;
+            case 9:msg.channel.send('絕對不是') ;break ;
+            case 10:msg.channel.send('也許') ;break ;
+            case 11:msg.channel.send('它將會帶來好運') ;break ;
+            case 12:msg.channel.send('你需要考慮其他方法') ;break ;
+            case 13:msg.channel.send('這不值得爭取') ;break ;
+            case 14:msg.channel.send('你的行為將會改善這件事') ;break ;
+            case 15:msg.channel.send('這是毫無疑問的') ;break ;
+            case 16:msg.channel.send('你必須現在行動') ;break ;
+            case 17:msg.channel.send('絕不') ;break ;
+            case 18:msg.channel.send('這不重要') ;break ;
+            case 19:msg.channel.sendy('毫無疑問') ;break ;
+            case 20:msg.channel.send('不') ;break ;
+            case 21:msg.channel.send('你看得太近了') ;break ;
+            case 22:msg.channel.send('尋找更多選擇') ;break ;
+            case 23:msg.channel.send('你不是真心在意') ;break ;
+            case 24:msg.channel.send('這具有重大意義') ;break ;
+            case 25:msg.channel.send('信你的直覺') ;break ;
+            case 26:msg.channel.send('船到橋頭自然直') ;break ;
+            case 27:msg.channel.send('船到橋頭自然沉') ;break ;
+            case 28:msg.channel.send('等待一個更好的時機') ;break ;
+            case 29:msg.channel.send('等待一個更好的時機') ;break ;
+            case 30:msg.channel.send('owo') ;break ;
 
         }}};
         function Radom(max,min) {
